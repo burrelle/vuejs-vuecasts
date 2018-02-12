9c62d3ee0de21a021dc8df4e9f72d7a1dd92d649
Evan Burrell <burrelle@oregonstate.edu>
2018-02-12 15:46:07 -0800

    Vuecast progress

--- learn-vue/main.js
+++ learn-vue/main.js
@@ -50,8 +50,26 @@ Vue.component('message-item', {
 	}
 });
 
+Vue.component('modal-item', {
+	template: `
+		<div class="modal is-active">
+				<div class="modal-background"></div>
+				<div class="modal-content">
+					<div class="box">
+						<slot></slot>
+					</div>
+				</div>
+				<button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+		</div>
+	`
+})
+
 
 
 new Vue({
-	el: '#root'
+	el: '#root',
+
+	data: {
+		showModal: false
+	}
 })
